99607e43e70eef8cafc9257a7255f3454532760f
Reis Ciaramitaro <rciaramitaro5@gmail.com>
2022-08-11 15:23:09 -0600

    Added intro to prime.c

---
+++ prime.c
@@ -0,0 +1,17 @@
+/*
+Author: Reis Ciaramitaro & Jeremiah Thompson
+Assignment Number: 2
+Date of Submission: 8/11/2022
+Name of this file: prime.c
+Short Description of contents:
+
+*/
+
+//include necessary C libs
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main( int argc, char *argv[] ) {
+}
