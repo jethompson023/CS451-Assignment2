507d9ef5501cdde0c43b4bb0d9d455e12f2c4f9c
Reis Ciaramitaro <rciaramitaro5@gmail.com>
2022-08-17 18:01:56 -0600

    A lot of stuff

---
+++ badanjekSpike2/prime.c
@@ -0,0 +1,95 @@
+// MAKE SURE YOU USE LONG LONG INT AN ALSO PRINT USING %llu to print it Otherwise
+//negative numbers will appear.
+#include"sigHandler.h"
+
+long unsigned int calculatedPrime = 123400003;
+void handler(int signum);
+int checkPrimeAndPrint(unsigned long int toCheck);
+void resume();
+void suspend();
+void terminate();
+
+long long prime;
+long long test;
+int process;
+int bool = 0;
+int check;
+
+int main(int argc, char* argv[]){
+
+// Set up handlers here
+    signal(sigStart, handler);
+    signal(sigSus, handler);
+    signal(sigExit, handler);
+
+    //generate random number based on process number given
+
+    process = atoi(argv[1]);
+
+    //printf("Process: %d\tPID: %d\n", process, getpid());
+    for(int i = 0; i <= process; i++) {
+        long  low = 10000;
+        long  max = 99999;
+        prime = (rand() % (max - low + 1)) + low;
+        prime = abs(prime * low * 10) + ((rand() % (max - low + 1)) + low);
+        //printf("%ld\n", prime);
+    }
+    test = prime;
+    while(1){
+        if(bool == 2)
+            return 0;
+    }
+}
+
+
+void handler(int signum){
+// Write handler code here.
+
+        if (signum == sigStart) {
+            bool =1;
+            resume();
+        }
+        if (signum == sigSus) {
+            bool = 0;
+            //resume();
+            //sleep(.4);
+            suspend();
+        }
+        if (signum == sigExit) {
+            //sleep(.3);
+            terminate();
+        }
+}
+
+void resume(){
+    //start
+    if (bool == 1) {
+        printf("Process %d: My PID is %d: I just got started. \n"
+               "I am starting with the number %llu to find the next prime number.\n", process, getpid(), test);
+    }
+
+    while(bool == 1){
+        test = test + 1;
+        check = 1;
+        for( int b = 2; b < test; b++){
+            if ((test % b) == 0) {
+                check = 0;
+                break;
+            }
+        }
+        if (check == 1) {
+            prime = test;
+        }
+
+    }
+}
+void suspend(){
+    printf("Process %d: my PID is %d: I am about to be suspended... Highest\n"
+           "prime number I found is %llu.\n",process, getpid(), prime);
+
+}
+void terminate(){
+    printf("Process %d: my PID is %d: I completed my task and I am exiting.\n"
+           "Highest prime number I found is %llu.\n",process, getpid(), prime);
+    bool = 2;
+}
---
+++ main.c
@@ -0,0 +1,276 @@
+#include "sigHandler.h"
+struct itimerval timer;
+struct sigaction sa;
+int inputArray[100];
+int inCounter = 0;
+int interval;
+int readyQueue[100];
+int first = 0;
+int NEW = 1;
+int OLD = -1;
+int currProc[4];
+int nextProcIndex = 0;
+int count = 0;
+
+int processCount = 0;
+int currRunningProcess = -1;
+int currAT;
+int currBT;
+int currPID;
+int isNew;
+
+int initProc(int processNumber){
+    int pid;
+    switch(processNumber) {
+        case 0:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "0", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 1:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "1", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 2:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "2", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 3:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "3", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 4:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "4", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 5:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "5", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 6:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "6", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 7:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "7", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 8:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "8", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        case 9:
+            if ((pid = fork()) < 0) {
+                printf("Fork Failed\n");
+            } else if (pid == 0) { //child
+
+                //printf("attempting to start prime.c\n");
+                char *args[] = {"./prime", "9", NULL};
+                execvp("./prime", args);
+
+            }
+            break;
+        default:
+            printf("Process limit exceeded!\n");
+    }
+    if(pid > 0){
+        return pid;
+    }
+}
+
+//readyQueue[processCount] = processNumber
+//readyQueue[processCount + 1] = arrivalTime
+//readyQueue[processCount + 2] = burstTime
+//readyQueue[processCount + 3] = isNew
+//readyQueue[processCount + 4] = PID
+void contextSwitch(int clock) {
+    printf("\nCONTEXT SWITCHING\n");
+
+
+
+    if (processCount >= (inCounter/3)*5) {
+        processCount = 0;
+        currRunningProcess = processCount;
+        printf("LOOPED THROUGH ALL PROCESSES\n");
+    }
+    if (readyQueue[currRunningProcess + 2] == 0) {
+        while (readyQueue[processCount + 2] <= 0) {
+            processCount = processCount + 5;
+            if (processCount >= (inCounter/3)*5) {
+                processCount = 0;
+                printf("ALL PROCESSES FINISHED, EXITING\n");
+                exit(0);
+            }
+        }
+    }
+        if (currRunningProcess != -1 && readyQueue[currRunningProcess + 2] != 0 && currRunningProcess != processCount) {
+            printf("SUSPENDING PROCESS %d\n", readyQueue[currRunningProcess]);
+            kill(readyQueue[currRunningProcess + 4], sigSus);
+        }
+        currRunningProcess = processCount;
+        if (readyQueue[currRunningProcess + 3] == NEW) {
+            printf("INITING PROC %d\n", readyQueue[currRunningProcess]);
+            readyQueue[currRunningProcess + 4] = initProc(readyQueue[currRunningProcess]); //PID
+            readyQueue[currRunningProcess + 3] = OLD;
+        }
+        printf("Scheduler: Time Now: %d second\n", clock);
+
+        printf("Scheduling to Process %d (PID %d) whose remaining time is %d\n",
+                readyQueue[currRunningProcess], readyQueue[currRunningProcess + 4], readyQueue[currRunningProcess + 2]);
+
+        usleep(3);
+        kill(readyQueue[currRunningProcess + 4], sigStart);
+        processCount = processCount + 5; //go to next process in the ready queue
+
+   
+}
+
+void saHandler(int signum) {
+    static int clock = 0;
+    int printed = 0;
+
+
+    //build the ready queue so basically a long term scheduler
+    //enables rest of process handling
+    //clock time <= arrival time and it already isnt in the ready queue, add to ready queue
+    for (int processNum = 0; processNum < inCounter; processNum = processNum + 3) {
+        if((inputArray[processNum+1] <= clock) && inputArray[processNum] != -1) {
+            readyQueue[count] = inputArray[processNum];
+            readyQueue[count + 1] = inputArray[processNum + 1];
+            readyQueue[count + 2] = inputArray[processNum + 2];
+            readyQueue[count + 3] = NEW;
+            inputArray[processNum] = -1;
+            if(printed == 0) {
+                printf("Scheduler: Time Now: %d seconds\n", clock);
+                printed = 1;
+            }
+            printf("New Process %d with remaining time of %d has arrived.\n",
+               readyQueue[count], readyQueue[count + 2]);
+            processNum = processNum + 3;
+            count = count + 5;
+        }
+    }
+    readyQueue[currRunningProcess + 2]--;
+    if (readyQueue[currRunningProcess + 2] == 0 && currRunningProcess != -1) {
+        printf("Terminating Process %d (PID %d) whose remaining time is %d\n",
+            readyQueue[currRunningProcess], readyQueue[currRunningProcess + 4], readyQueue[currRunningProcess + 2]);
+        kill(readyQueue[currRunningProcess + 4], sigExit);
+    }
+        
+    if (clock % interval == 0 && readyQueue[0] != -1)
+        contextSwitch(clock);
+    clock++;
+}
+
+int main(int argc, char* argv[]) {
+    interval = atoi(argv[2]);
+    //initialize readyQueue
+    for(int r = 0; r < 100; r++){
+        readyQueue[r] = -1;
+    }
+
+    //open input file
+    FILE* inFile;
+    inFile = fopen(argv[1], "r");
+    if (inFile == NULL){
+        printf("error oppening file\n");
+        //sys exit
+    }
+    else{
+        printf("input file opened\n");
+    }
+
+    //reading input file
+    while(!feof(inFile)){
+        fscanf(inFile, "%d", &inputArray[inCounter]);
+        fscanf(inFile, "%d", &inputArray[inCounter + 1]);
+        fscanf(inFile, "%d", &inputArray[inCounter + 2]);
+        printf("PID: %d    AT: %d    BT: %d\n",
+               inputArray[inCounter], inputArray[inCounter+1], inputArray[inCounter+2]);
+        inCounter = inCounter + 3;
+    }
+
+    //close input file
+    fclose(inFile);
+
+    //starting SIGALRM handler saHandler
+    //memset (&sa, 0, sizeof (sa));
+    sa.sa_handler = &saHandler;
+    sigaction (SIGALRM, &sa, NULL);
+
+    //initializing timer variables
+    timer.it_interval.tv_sec = 1;
+    timer.it_interval.tv_usec = 0;
+    timer.it_value.tv_sec = 1;
+    timer.it_value.tv_usec = 0;
+
+    //starting timer
+    setitimer (ITIMER_REAL, &timer, NULL);
+    while(1) {
+
+    }
+
+    return 0;
+}
